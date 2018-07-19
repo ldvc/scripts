a25b3c315c68b838c7f840faa724ba6b55167bd2
ldvc <ludovic+github@terrier.im>
2018-07-19 17:13:57 +0200

    Gestion de plusieurs enfants

--- merge-pdf.sh
+++ merge-pdf.sh
@@ -4,21 +4,29 @@ set -euo pipefail
 IFS=$'\n\t'
 
 REAL_USER=$(who -m | cut -d " " -f 1)
-KID_NAME="toto"
-NC_DIR="/data/nextcloud-data/$REAL_USER/files/Documents/$KID_NAME/CRECHE/"
+KIDS=(luke leia)
 
-echo ""
-files=$(find $NC_DIR -type f -mmin -60)
+for kid in ${KIDS[*]}; do
 
-echo "[x] Fichiers trouvés :"
-for file in $files ; do
-  short=$(echo $file | cut -d "/" -f 8-9)
-  echo -e "    $short"
+  NC_DIR="/data/nextcloud-data/$REAL_USER/files/Documents/$kid/CRECHE/"
+  echo ""
+  files=$(find "$NC_DIR" -type f -mmin -60)
+
+  if [ -z "$files" ] ; then
+    echo "[ ] Aucun fichier trouvé pour $kid !"
+    exit 10
+  else
+    echo "[x] Fichiers trouvés pour $kid :"
+    for file in $files ; do
+      short=$(echo "$file" | cut -d "/" -f 8-9)
+      echo -e "    $short"
+    done
+  fi
+
+  #pdftk ${files[@]} cat output output.pdf
+  echo "[x] Fichiers PDF fusionnés"
+  echo -e "\n======================================================="
+  echo "Actions à effectuer"
+  echo "[ ] $ mv output.pdf 2018_JUIN_fusion_creche_$kid.pdf"
+  echo "[ ] $ cp 2018_JUIN_fusion_creche_$kid.pdf ~/openbar/."
 done
-
-pdftk ${files[@]} cat output output.pdf
-echo "[x] Fichiers PDF fusionnés"
-echo -e "\n======================================================="
-echo "Actions à effectuer"
-echo "[ ] $ mv output.pdf 2018_JUIN_fusion_creche_$KID_NAME.pdf"
-echo "[ ] $ cp 2018_JUIN_fusion_creche_$KID_NAME.pdf ~/openbar/."
